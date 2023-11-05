7072fc9d2807f0f8398fbf5174b7f1dc4fd07703
Brian McConeghy <brianmcconeghy@gmail.com>
2023-11-05 14:02:03 -0800

    Add Black formatting

--- datatypes.py
+++ datatypes.py
@@ -8,7 +8,9 @@ from typing import Optional
 
 converter = Converter()
 converter.register_unstructure_hook(datetime, lambda d: str(d))
-converter.register_structure_hook(datetime, lambda d, _: datetime.strptime(d, "%Y-%m-%d").date())
+converter.register_structure_hook(
+    datetime, lambda d, _: datetime.strptime(d, "%Y-%m-%d").date()
+)
 
 
 @attr.define
@@ -23,19 +25,27 @@ class JobPosting:
         article = job_posting.body.article.contents[1]
 
         salary_range_tuple = None
-        salary_description = [s.string for s in job_posting.find_all("p") if s.string and "hiring range" in s.string]
+        salary_description = [
+            s.string
+            for s in job_posting.find_all("p")
+            if s.string and "hiring range" in s.string
+        ]
         if salary_description:
             salary_range = re.findall(r"\$\d+,\d+", salary_description[0])
             # Convert salary range to tuple of integers representing low and high end of range
-            salary_range_tuple = tuple([int(re.sub("\$|\,", "", s)) for s in salary_range]) or None
+            salary_range_tuple = (
+                tuple([int(re.sub("\$|\,", "", s)) for s in salary_range]) or None
+            )
 
         return cls(
             role=job_posting.h1.string,
-            date=datetime.strptime(article.find("span", class_="date").string, "%B %d, %Y").date(),
+            date=datetime.strptime(
+                article.find("span", class_="date").string, "%B %d, %Y"
+            ).date(),
             id=article.find("span", class_="id").string.split(": ")[1],
             salary_range=salary_range_tuple,
         )
-    
+
     @property
     def salary_low_end(self) -> Optional[int]:
         try:
@@ -48,4 +58,4 @@ class JobPosting:
         try:
             return self.salary_range[1]
         except Exception:
-                return None
+            return None
--- main.py
+++ main.py
@@ -12,10 +12,11 @@ from datatypes import converter, JobPosting
 
 def get_html(url: str) -> BeautifulSoup:
     html = requests.get(url)
-    soup = BeautifulSoup(html.text, 'html.parser')
+    soup = BeautifulSoup(html.text, "html.parser")
     return soup
 
-def fetch_job_pages(links: list[str]) -> list[Tag]:    
+
+def fetch_job_pages(links: list[str]) -> list[Tag]:
     """Fetch all content from job postings."""
     job_pages = []
     for link in links:
@@ -40,34 +41,54 @@ def plot_salary_ranges(df: pd.DataFrame, salary_to_benchmark_against: int):
     df = df.dropna()
     df["Low End"] = df["salary_range"].apply(lambda x: x[0])
     df["High End"] = df["salary_range"].apply(lambda x: x[1])
-    fig = px.bar(df.sort_values("Low End", ascending=False), y="role", x=["High End", "Low End"], barmode="group", labels={"variable": "Salary", "role": "Role", "value": "$"})
-    fig = fig.update_layout(title=dict(text="Salaries at AbCellera (Job Postings)", font=dict(size=30)))
-    fig.add_vline(x=salary_to_benchmark_against, line_width=3, line_dash="dash", line_color="green")
+    fig = px.bar(
+        df.sort_values("Low End", ascending=False),
+        y="role",
+        x=["High End", "Low End"],
+        barmode="group",
+        labels={"variable": "Salary", "role": "Role", "value": "$"},
+    )
+    fig = fig.update_layout(
+        title=dict(text="Salaries at AbCellera (Job Postings)", font=dict(size=30))
+    )
+    fig.add_vline(
+        x=salary_to_benchmark_against,
+        line_width=3,
+        line_dash="dash",
+        line_color="green",
+    )
     fig.add_annotation(
-        x=salary_to_benchmark_against, 
+        x=salary_to_benchmark_against,
         y=1,
         yref="paper",
         showarrow=True,
         align="center",
         arrowhead=2,
         arrowsize=1,
-        arrowwidth=2, text="Salary to compare")
+        arrowwidth=2,
+        text="Salary to compare",
+    )
     return fig
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     DEFAULT_SALARY = 50000
-    user_salary = input(f"Please enter salary to benchmark against (in integer format, e.g. 50000 for $50,000)\n(Press ENTER to set benchmark salary to ${DEFAULT_SALARY:,})\n") or DEFAULT_SALARY
+    user_salary = (
+        input(
+            f"Please enter salary to benchmark against (in integer format, e.g. 50000 for $50,000)\n(Press ENTER to set benchmark salary to ${DEFAULT_SALARY:,})\n"
+        )
+        or DEFAULT_SALARY
+    )
     job_postings_path = Path("job_postings.json")
     if not job_postings_path.exists():
         url = "https://abcellera.com/careers-openings/"
         careers_page = get_html(url=url)
         links = careers_page.find_all("a")
         job_page_links = [
-            f"{url}{link.get('href')}" 
-            for link in links 
+            f"{url}{link.get('href')}"
+            for link in links
             if link.string and "View position" in link.string
-            ]
+        ]
         job_pages = fetch_job_pages(job_page_links)
         job_postings = structure_job_postings(job_pages)
         with open("job_postings.json", "w") as fh:
